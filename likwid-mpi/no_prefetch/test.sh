fa718a4bc8d3d3ea4bea0a4465f0c30d27d5dc40
Onkar Patil <opatil@ncsu.edu>
2020-03-02 16:14:42 -0500

    new file:   likwid-mpi/no_prefetch/1w4r/Makefile 	new file:   likwid-mpi/no_prefetch/1w4r/numatest_mpi.h 	new file:   likwid-mpi/no_prefetch/1w4r/numatest_mpi_np.c 	new file:   likwid-mpi/no_prefetch/1w4r/test3.c 	new file:   likwid-mpi/no_prefetch/1w4r/test_numa.sh 	new file:   likwid-mpi/no_prefetch/27sten/Makefile 	new file:   likwid-mpi/no_prefetch/27sten/numatest_mpi.h 	new file:   likwid-mpi/no_prefetch/27sten/numatest_mpi_np.c 	new file:   likwid-mpi/no_prefetch/27sten/test3.c 	new file:   likwid-mpi/no_prefetch/27sten/test_numa.sh 	new file:   likwid-mpi/no_prefetch/3sten/Makefile 	new file:   likwid-mpi/no_prefetch/3sten/numatest_mpi.h 	new file:   likwid-mpi/no_prefetch/3sten/numatest_mpi_np.c 	new file:   likwid-mpi/no_prefetch/3sten/test3.c 	new file:   likwid-mpi/no_prefetch/3sten/test_numa.sh 	new file:   likwid-mpi/no_prefetch/5sten/Makefile 	new file:   likwid-mpi/no_prefetch/5sten/numatest_mpi.h 	new file:   likwid-mpi/no_prefetch/5sten/numatest_mpi_np.c 	new file:   likwid-mpi/no_prefetch/5sten/test3.c 	new file:   likwid-mpi/no_prefetch/5sten/test_numa.sh 	new file:   likwid-mpi/no_prefetch/7sten/Makefile 	new file:   likwid-mpi/no_prefetch/7sten/numatest_mpi.h 	new file:   likwid-mpi/no_prefetch/7sten/numatest_mpi_np.c 	new file:   likwid-mpi/no_prefetch/7sten/test3.c 	new file:   likwid-mpi/no_prefetch/7sten/test_numa.sh 	new file:   likwid-mpi/no_prefetch/9sten/Makefile 	new file:   likwid-mpi/no_prefetch/9sten/numatest_mpi.h 	new file:   likwid-mpi/no_prefetch/9sten/numatest_mpi_np.c 	new file:   likwid-mpi/no_prefetch/9sten/test3.c 	new file:   likwid-mpi/no_prefetch/9sten/test_numa.sh 	new file:   likwid-mpi/no_prefetch/l3cache/Makefile 	new file:   likwid-mpi/no_prefetch/l3cache/numatest_mpi.h 	new file:   likwid-mpi/no_prefetch/l3cache/numatest_mpi_np.c 	new file:   likwid-mpi/no_prefetch/l3cache/test3.c 	new file:   likwid-mpi/no_prefetch/l3cache/test_numa.sh 	new file:   likwid-mpi/no_prefetch/rand/Makefile 	new file:   likwid-mpi/no_prefetch/rand/numatest_mpi.h 	new file:   likwid-mpi/no_prefetch/rand/numatest_mpi_np.c 	new file:   likwid-mpi/no_prefetch/rand/test3.c 	new file:   likwid-mpi/no_prefetch/rand/test_numa.sh 	new file:   likwid-mpi/no_prefetch/str/Makefile 	new file:   likwid-mpi/no_prefetch/str/numatest_mpi.h 	new file:   likwid-mpi/no_prefetch/str/numatest_mpi_np.c 	new file:   likwid-mpi/no_prefetch/str/test3.c 	new file:   likwid-mpi/no_prefetch/str/test_numa.sh 	new file:   likwid-mpi/no_prefetch/test.sh 	modified:   likwid-mpi/no_prefetch/test_numa.sh 	new file:   likwid-mpi/no_prefetch/wr-only/Makefile 	new file:   likwid-mpi/no_prefetch/wr-only/numatest_mpi.h 	new file:   likwid-mpi/no_prefetch/wr-only/numatest_mpi_np.c 	new file:   likwid-mpi/no_prefetch/wr-only/test3.c 	new file:   likwid-mpi/no_prefetch/wr-only/test_numa.sh

---
+++ likwid-mpi/no_prefetch/test.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/bash
+
+cd wr-only
+sh test_numa.sh
+cd 1w4r
+sh test_numa.sh
+cd str
+sh test_numa.sh
+cd rand
+sh test_numa.sh
+cd l3cache
+sh test_numa.sh
+cd 3sten
+sh test_numa.sh
+cd 5sten
+sh test_numa.sh
+cd 7sten
+sh test_numa.sh
+cd 9sten
+sh test_numa.sh
+cd 27sten
+sh test_numa.sh
