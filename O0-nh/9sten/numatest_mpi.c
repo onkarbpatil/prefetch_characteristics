430d1889a70998846cdcfd1ca2832f9a92941ffa
Onkar Patil <opatil@ncsu.edu>
2020-05-15 15:11:42 -0400

    modified:   O0-nh/1w4r/numatest_mpi.c 	modified:   O0-nh/1w4r_nvm/numatest_mpi.c 	modified:   O0-nh/27sten/numatest_mpi.c 	modified:   O0-nh/27sten_nvm/numatest_mpi.c 	modified:   O0-nh/3sten/numatest_mpi.c 	modified:   O0-nh/3sten_nvm/numatest_mpi.c 	modified:   O0-nh/5sten/numatest_mpi.c 	modified:   O0-nh/5sten_nvm/numatest_mpi.c 	modified:   O0-nh/7sten/numatest_mpi.c 	modified:   O0-nh/7sten_nvm/numatest_mpi.c 	modified:   O0-nh/9sten/numatest_mpi.c 	modified:   O0-nh/9sten_nvm/numatest_mpi.c 	modified:   O0-nh/numa_fm_new.csv 	modified:   O0-nh/wr-only/numatest_mpi.c

--- O0-nh/9sten/numatest_mpi.c
+++ O0-nh/9sten/numatest_mpi.c
@@ -62,13 +62,13 @@ void numatest(int argc, char ** argv, int rank, int procs, unsigned long bytes){
   	i = 0;
 //	while(i < total_numa_nodes){
 	{
-wr_dist = 64/sizeof(double);
+wr_dist = 512/sizeof(double);
 //while(wr_dist < 32768/sizeof(double)){
 {
-	rd_dist = 1024/sizeof(double);
+	rd_dist = 8192/sizeof(double);
 //		while(rd_dist < 32768/sizeof(double)){
 	{
-				unroll = 64;
+				unroll = 8;
 //				while(unroll < 128){
 				{
 	// Dynamically allocate the three arrays using "posix_memalign()"
